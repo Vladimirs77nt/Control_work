c05dc8b06124a44352d1590b1f48bd99163c9a4c
Your NameVladimirs77nt <vladimirs77@yandex.ru>
2023-05-01 08:40:19 +0500

    Продолжение

--- toy_store.java
+++ toy_store.java
@@ -28,3 +28,70 @@
 В 20% двойка
 И в 60% тройка. */
 
+import java.util.PriorityQueue;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Toy_store {
+
+    public static void main(String[] args) {
+        System.out.println("Программа запущена");
+        ArrayList<Product> prizes = new ArrayList<>();
+        prizes.add(new Product(1, "Конструктор", 20));
+        prizes.add(new Product(2, "Медвежонок", 30));
+        prizes.add(new Product(3, "Самолетик", 40));
+        prizes.add(new Product(4, "Кукла", 25));
+        int probability = 0;
+        for (Product i : prizes) {
+            System.out.println(i.toString());
+            probability += i.getProbability();
+        }
+
+        System.out.println(probability);
+
+
+        PriorityQueue<Product> basket_of_prizes = new PriorityQueue<>();
+    }
+}
+
+class Product{
+    private int id;          // ID номер
+    private String name;     // название игрушки
+    private int probability; // частота выпадания, шанс
+    
+    public Product(int id, String name, int probability) {
+        this.id = id;
+        this.name = name;
+        this.probability = probability;
+    }
+
+    @Override
+    public String toString() {
+        return "Приз {" + "id = " + id + ", " + name + ", вероятность: " + probability + "}";
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getProbability() {
+        return probability;
+    }
+
+    public void setProbability(int probability) {
+        this.probability = probability;
+    }
+  
+}
